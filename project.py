0b0ac1e63625d03086837ec6c0fe1f144fb8ebf7
randollz <randallhengyr18@gmail.com>
2026-03-11 12:25:41 +0800

    added a new function

--- project.py
+++ project.py
@@ -1,6 +1,4 @@
 print("Hello world, how are you today")
-<<<<<<< HEAD
-=======
 print("Thats great, Im good aswell")
 print("Hello")
 
@@ -9,4 +7,3 @@ y = 4
 z = 2 + 4
 print(z)
 
->>>>>>> c5636971c4f3203c7810b617a9898225709bb0c9
